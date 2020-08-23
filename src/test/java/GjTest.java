1f8c64c69116a8fe4ae8b6a9a8a8cb13fb9a7f83
gujing04 <gujing04@meituan.com>
2020-08-24 00:06:25 +0800

    first commit

---
+++ src/test/java/GjTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.Test;
+
+/**
+ * Created by GuJing04 on 2020/8/23 下午11:51.
+ */
+public class GjTest {
+
+    @Test
+    public void test () {
+        System.out.println("ll");
+    }
+}
